329f023c97b4c080e38fdb3d710fb9739998f0d5
Lily Li <lilyzhili@gmail.com>
2017-02-06 15:47:20 -0700

    final

---
+++ app/src/main/java/com/example/zhi6_sizebook/NewPersonActivity.java
@@ -0,0 +1,8 @@
+package com.example.zhi6_sizebook;
+
+/**
+ * Created by Zhi Li on 2017/2/5.
+ */
+
+public class NewPersonActivity {
+}
---
+++ app/src/main/java/com/example/zhi6_sizebook/Person.java
@@ -0,0 +1,8 @@
+package com.example.zhi6_sizebook;
+
+/**
+ * Created by Zhi Li on 2017/2/2.
+ */
+
+public class Person {
+}
